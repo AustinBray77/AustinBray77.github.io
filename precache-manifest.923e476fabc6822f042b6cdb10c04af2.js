0a478e451fcef785e3314a20fdec745a8c95545f
deoignition <93416177+AustinBray77@users.noreply.github.com>
2022-08-14 17:23:02 -0400

    Updates to website, check parallel repo

---
+++ precache-manifest.923e476fabc6822f042b6cdb10c04af2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89a1dc7890849acdf1c1",
+    "url": "/static/css/main.b9be3814.chunk.css"
+  },
+  {
+    "revision": "89a1dc7890849acdf1c1",
+    "url": "/static/js/main.36cdf327.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8e03c945262f0aef7d7c",
+    "url": "/static/css/2.5a223140.chunk.css"
+  },
+  {
+    "revision": "8e03c945262f0aef7d7c",
+    "url": "/static/js/2.b7085761.chunk.js"
+  },
+  {
+    "revision": "6f0035724fc052fe18707f55054d1811",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3e2e89e0faefe4e046418c7feb9be694.js"
+  "/precache-manifest.923e476fabc6822f042b6cdb10c04af2.js"
 );
 
 workbox.clientsClaim();
