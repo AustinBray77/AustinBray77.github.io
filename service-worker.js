6869f050452d78484afb98b13ee23fa6c1b17601
deoignition <93416177+AustinBray77@users.noreply.github.com>
2022-08-14 20:51:48 -0400

    Fixed images on blog

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.923e476fabc6822f042b6cdb10c04af2.js"
+  "/precache-manifest.0752f698e185f5d851e13bb76f4eba85.js"
 );
 
 workbox.clientsClaim();
