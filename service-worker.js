c72f73c0848d4ca8e43e545294b8cac37c1b4dfa
deoignition <93416177+AustinBray77@users.noreply.github.com>
2022-09-17 23:32:21 -0400

    Updates To Site: - Added Article "Cracking The Coding Interview" - QOL Changes to Totris as well as minor bug fixes

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6da27d3bf52e7f5215ad266bf6319162.js"
+  "/precache-manifest.4f5060ea3e99b88ac5c4efd4bd383d23.js"
 );
 
 workbox.clientsClaim();
